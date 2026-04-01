7b81354fb5971185609b6eb98400bb828ecf16e5
Kushal patil <kushalpatil@MacBook-Air-6.local>
2026-04-01 16:22:57 -0400

    added files and improved organization

--- 03_md/dat_to_bsheetpct.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cd45449b79a93475441fdec2ce8cc20176158633714e2dbf61e000dc2ad0b75b
-size 1213
---
+++ statistical tests/barplot_dE.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:421c850867846841bb24d63794f847ebad1b0268f9bcc764421b9bd70aaef85b
+size 879
