21bf9f257008a9cdb7b127b45c757b58684c68c5
kpis-him <138505630+kpis-him@users.noreply.github.com>
2026-05-14 17:41:20 -0400

    Update variables

--- Analysis Scripts/Beta_sheet_composition.py
+++ Analysis Scripts/Beta_sheet_composition.py
@@ -59,10 +59,11 @@ def delta_e():
     delta_E_UCCSD = [0.01983, -0.00861, -0.0486]  
     delta_E_TwoLocal = [-0.161, -0.338, 0.0772]  
     delta_E_EfficientSU2 = [-0.11]  
-    delta_E_B3LYP = [0.02, -0.01, -0.05]  
+    delta_E_B3LYP = [0.02, 0.03, 0.01]
+    delta_E_CASCI = [0.04, 0.02, 0.01]
 
     h_stat, p_value = kruskal(delta_E_UCCSD, delta_E_TwoLocal,
-                          delta_E_EfficientSU2, delta_E_B3LYP)
+                          delta_E_EfficientSU2, delta_E_B3LYP, delta_E_CASCI)
 
     print(f"p-value: {p_value}")
 #dssp_blah()
